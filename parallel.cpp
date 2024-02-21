e23efc403cfda42e9ccd3ae01cbfe5a14456b2c3
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-02-21 02:04:02 -0500

    added basic timing for threads

--- parallel.cpp
+++ parallel.cpp
@@ -2,21 +2,26 @@
 #include <unistd.h>
 #include <thread>
 #include <vector>
-#include <ctime>
+#include <chrono>
 
 #define MAX_THREADS 4
 
-void matrixMult(int source1[][4], int source2[][4], int dest[][4], int i){
-    for(int j=0;j<4;j++){
-        for(int k=0;k<4;k++){
+const int size=4;
+void matrixMult(int source1[][size], int source2[][size], int dest[][size], int i){
+    for(int j=0;j<size;j++){
+        for(int k=0;k<size;k++){
             dest[i][j]+=source1[i][k]*source2[k][j];
         }
     }
 }
 
 int main(int argc, char* argv[]){
+    //borrowed timing execution code from https://stackoverflow.com/questions/22387586/measuring-execution-time-of-a-function-in-c
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
     srand(time(NULL));
-    const int size=4;
     int A[size][size]={};
     int B[size][size]={};
     int C1[size][size]={};
@@ -32,6 +37,7 @@ int main(int argc, char* argv[]){
     int sizeA=sizeof(A)/sizeof(A[0]);
     int sizeB=sizeof(B)/sizeof(B[0]);
     int sizeC=sizeof(A[0])/sizeof(int);
+    auto t1 = high_resolution_clock::now();
     for(int row=0;row<sizeA;row++){
         for(int col=0;col<sizeB;col++){
             total=0;
@@ -42,6 +48,17 @@ int main(int argc, char* argv[]){
             C1[row][col]=total;
         }
     }
+    auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
+
     std::cout<<"First Matrix"<<std::endl;
     for (int i=0;i<size;i++){
         for(int j=0;j<size;j++){
@@ -63,7 +80,7 @@ int main(int argc, char* argv[]){
         }
         std::cout<<"\n";
     }
-
+    t1 = high_resolution_clock::now();
     std::thread threads[MAX_THREADS];
     for(int i=0;i<MAX_THREADS;i++){
         threads[i]=std::thread(matrixMult,std::ref(A),std::ref(B),std::ref(C2),i);
@@ -71,6 +88,11 @@ int main(int argc, char* argv[]){
     for(int i=0;i<MAX_THREADS;i++){
         threads[i].join();
     }
+    t2 = high_resolution_clock::now();
+    ms_int = duration_cast<milliseconds>(t2 - t1);
+    ms_double = t2 - t1;
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
     std::cout<<"C2 Matrix"<<std::endl;
     for (int i=0;i<size;i++){
         for(int j=0;j<size;j++){
